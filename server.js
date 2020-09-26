7767f5fc12325b677fd29b21f7bdbbba5b09263c
Perla Coutinho <perla.coutinho@gmail.com>
2020-09-26 13:28:21 -0300

    configuração da port http get

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 'use strict'
 
-console.log('Testando...');
+const http = require('http');
+const debug = require('debug')('opsmanager:server');
+const express = require('express');
+const bodypaser = require('body-parser');
+
+
+const app = express();
+const port = normalizarPort(process.env.PORT || '3000');
+app.set('port', port);
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/',(req,res,next) =>{
+    res.status(200).send({
+        title: "operations manager API",
+        version: "0.0.1"
+    });
+})
+
+app.use('/',route);
+
+server.listen(port);
+console.log("API rodando na porta " + port);
+
+function normalizarPort(val){
+    const port = parseInt(val,10);
+
+    if(isNaN(port)){
+        return val;
+    }    
+    if(port >= 0){
+        return port;
+    }
+    return false;
+}
